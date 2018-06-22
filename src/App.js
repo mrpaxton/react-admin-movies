c393098bdfb7e904554c8ee99e242ea95e102846
Sarn Wattanasri <mr.paxton@gmail.com>
2018-06-22 12:47:31 -0700

    Included the Create and Edit functionalities in posts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
-import { PostList } from './posts';
+import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
         <Resource name="users" list={UserList} />
     </Admin>
 );
--- src/posts.js
+++ src/posts.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
+import { Create, Edit, List, Datagrid, TextField, ReferenceField, EditButton, DisabledInput, LongTextInput, ReferenceInput, SelectInput, SimpleForm, TextInput  } from 'react-admin';
 
 export const PostList = (props) => (
     <List {...props}>
@@ -11,6 +11,37 @@ export const PostList = (props) => (
             </ReferenceField>
             <TextField source="title" />
             <TextField source="body" />
+            <EditButton />
         </Datagrid>
     </List>
+);
+
+const PostTitle = ({record}) => (
+    <span>Post title: { record ? `"${ record.title }"` : '' }</span>
+);
+
+export const PostEdit = (props) => (
+    <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <ReferenceInput label="Post's User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+
+export const PostCreate = (props) => (
+    <Create title="Let's create a post" {...props}>
+        <SimpleForm>
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Create>
 );
