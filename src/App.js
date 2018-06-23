c5633a1f5f842d5baba8cf893c5e2e72b62fb9a9
Sarn Wattanasri <mr.paxton@gmail.com>
2018-06-23 00:25:44 -0700

    Created a custom Dashboard page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import UserIcon from '@material-ui/icons/Group';
 import { Admin, Resource } from 'react-admin';
 import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
+import Dashboard from './Dashboard';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
         <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
---
+++ src/Dashboard.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+
+export default () => (
+    <Card>
+        <CardHeader title="Hello Admin!" />
+        <CardContent>This is an example of a dashboard using react-admin</CardContent>
+    </Card>
+);
+
