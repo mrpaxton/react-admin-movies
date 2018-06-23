b49535fc8cc51a6a88b42557f2094c123527d8f6
Sarn Wattanasri <mr.paxton@gmail.com>
2018-06-23 00:15:30 -0700

    Menu Icons using material design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import React from 'react';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 import { Admin, Resource } from 'react-admin';
 import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
@@ -8,8 +10,8 @@ import jsonServerProvider from 'ra-data-json-server';
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-        <Resource name="users" list={UserList} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
 );
 
