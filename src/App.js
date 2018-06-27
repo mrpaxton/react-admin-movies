5dcadc144bc17aa16271d1c98b6dcb7e8c238208
Sarn Wattanasri <mr.paxton@gmail.com>
2018-06-26 18:19:42 -0700

    Added customRoutes to App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import UserIcon from '@material-ui/icons/Group';
 import { Admin, Resource } from 'react-admin';
 import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
-import Dashboard from './Dashboard';
+import Menu from './Menu';
+import customRoutes from './customRoutes';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Admin menu={Menu} customRoutes={customRoutes} dataProvider={dataProvider}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
         <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
